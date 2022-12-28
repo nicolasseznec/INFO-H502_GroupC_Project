06e4478a3723f590df9e9d355827823abdcfc4fb
Nicolas Seznec <seznec.nicolas@yahoo.fr>
2022-12-28 19:37:00 +0100

    Moved debug tools to Debug.h

--- src/camera.h
+++ src/camera.h
@@ -5,7 +5,7 @@
 #include <GLFW/glfw3.h>
 
 #include <glm/glm.hpp>
-#include<glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
