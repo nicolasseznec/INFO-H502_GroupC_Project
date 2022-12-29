786b049c6b0be83a11b301b9cc4d7bb6296d80e9
Nicolas Seznec <seznec.nicolas@yahoo.fr>
2022-12-29 17:21:19 +0100

    Removed commented code : skybox.h

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#include <vector>
+#include <map>
 
 #include "camera.h"
 #include "shader.h"
--- src/skybox.h
+++ src/skybox.h
@@ -6,9 +6,6 @@
 
 #include <glad/glad.h>
 
-// #define STB_IMAGE_IMPLEMENTATION
-// #include "stb_image.h"
-
 #include "shader.h"
 #include "object.h"
 
@@ -27,8 +24,6 @@ public:
     ) : cubeMap(cubePath) {
         glGenTextures(1, &cubeMapTexture);
         bindTexture();
-        // glActiveTexture(GL_TEXTURE0);
-        // glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexture);
 
         // texture parameters
         glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -41,30 +36,11 @@ public:
 
         std::string pathToCubeMap = PATH_TO_TEXTURE "/cubemaps/yokohama3/";
 
-        // std::map<std::string, GLenum> facesToLoad;
-        // std::map<std::string, GLenum> facesToLoad = {
-        //     {pathToCubeMap + faces[0].c_str(), GL_TEXTURE_CUBE_MAP_POSITIVE_X},
-        //     {pathToCubeMap + posY, GL_TEXTURE_CUBE_MAP_POSITIVE_Y},
-        //     {pathToCubeMap + posZ, GL_TEXTURE_CUBE_MAP_POSITIVE_Z},
-        //     {pathToCubeMap + negX, GL_TEXTURE_CUBE_MAP_NEGATIVE_X},
-        //     {pathToCubeMap + negY, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y},
-        //     {pathToCubeMap + negZ, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z},
-        // };
-
-        // for (int i = 0; i < faces.size(); i++) {
-        //     facesToLoad[pathToCubeMap + faces[i].c_str()] = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
-        // }
-
         //load the six faces
         for (std::pair<std::string, GLenum> pair : faces) {
-        // for (int i = 0; i < faces.size(); i++) {
             loadCubemapFace((path + pair.first).c_str(), pair.second);
         }
-
-        // char pathCube[] = PATH_TO_OBJECTS "/cube.obj";
-	    // cubeMap = Object(pathCube);
 	    cubeMap.makeObject(shader);
-
     }
 
     void loadCubemapFace(const char* path, const GLenum& targetFace)
